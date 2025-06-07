45115ff93a0bea3876df4bf601867b7ac83c3e5a
Samuel Edward Howells <samhowells86@gmail.com>
2025-06-07 17:29:00 +0100

    Add spiral_optimize and tests

--- src/tsal/tools/brian/__init__.py
+++ src/tsal/tools/brian/__init__.py
@@ -1,5 +1,5 @@
 """Brian Spiral Code Healer tools."""
 
-from .optimizer import SymbolicOptimizer, analyze_and_repair
+from .optimizer import SymbolicOptimizer, analyze_and_repair, spiral_optimize
 
-__all__ = ["SymbolicOptimizer", "analyze_and_repair"]
+__all__ = ["SymbolicOptimizer", "analyze_and_repair", "spiral_optimize"]
