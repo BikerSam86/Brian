491f6d6e747d4e2eef386f6ca3e9b3fb0a14c0d4
Samuel Edward Howells <samhowells86@gmail.com>
2025-06-08 01:33:19 +0100

    enhance mode switching

--- tests/unit/test_core/test_tsal_executor.py
+++ tests/unit/test_core/test_tsal_executor.py
@@ -1,4 +1,5 @@
-from tsal.core.tsal_executor import TSALExecutor, TSALOp, SpiralVector
+from tsal.core.tsal_executor import TSALExecutor, TSALOp, SpiralVector, ExecutionMode
+from tsal.core.executor import MetaFlagProtocol
 
 
 def test_cycle_creates_nodes():
@@ -19,3 +20,28 @@ def test_spiral_depth_increase():
     ]
     tvm.execute(program)
     assert tvm.spiral_depth == 2
+
+
+def test_mode_trace():
+    tvm = TSALExecutor(MetaFlagProtocol(narrative_mode=True))
+    program = [
+        (TSALOp.INIT, {'mesh': True}),
+    ]
+    tvm.execute(program)
+    assert tvm.mode == ExecutionMode.TRACE
+
+
+def test_mode_fork():
+    tvm = TSALExecutor(MetaFlagProtocol(fork_tracking=True))
+    program = [
+        (TSALOp.FORGE, {'fork': True}),
+    ]
+    tvm.execute(program)
+    assert tvm.mode == ExecutionMode.FORK
+
+
+def test_mode_arm_on_error():
+    tvm = TSALExecutor()
+    tvm.error_mansion.append({'type': 'test'})
+    tvm._switch_mode(0)
+    assert tvm.mode == ExecutionMode.ARM
